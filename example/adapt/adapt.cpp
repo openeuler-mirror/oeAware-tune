abd9fa539173ebe5bd09b363469a17b5d2f85593
zhoukaiqi <zhoukaiqi@huawei.com>
2024-05-31 11:28:59 +0800

    example adapts to new interface

--- example/adapt/adapt.cpp
+++ example/adapt/adapt.cpp
@@ -17,73 +17,83 @@
 #include "tune.h"
 #include <cstring>
 uint64_t g_scenario_buf_cnt = 0;
-char *get_version()
+
+const char *get_version()
 {
     return "v1.0";
 }
 
-char *get_description()
+const char *get_name()
 {
     return "tune_example";
 }
 
-char *get_name()
+const char *get_description()
 {
     return "tune_example";
 }
 
-char *get_dep()
+const char *get_dep()
 {
     return "scenario_example";
 }
 
-int get_cycle()
+int get_priority()
+{
+    return 2;
+}
+
+int get_period()
 {
     return 1000;
 }
 
-void enable()
+bool enable()
 {
+    return true;
 }
+
 void disable()
 {
 }
 
-void tune(void *info[], int len)
+void run(const struct Param *para)
 {
 
-    for (int i = 0; i < len; i++) {
-        struct DataHeader *header = (struct DataHeader *)info[i];
-        if (strcmp(header->type, SCENARIO_ACCESS_BUF) == 0) {
-            int dataNum = std::min(header->count - g_scenario_buf_cnt, (uint64_t)header->buf_len);
+    for (int i = 0; i < para->len; i++) {
+        struct DataRingBuf *ringbuf = (struct DataRingBuf *)para->ring_bufs[i];
+        if (strcmp(ringbuf->instance_name, SCENARIO_ACCESS_BUF) == 0) {
+            int dataNum = std::min(ringbuf->count - g_scenario_buf_cnt, (uint64_t)ringbuf->buf_len);
             if (dataNum != 1) { // demo only support one data
                 return;
             }
             struct DataBuf *buf = nullptr;
-            int offset = (header->index + header->buf_len - 0) % header->buf_len;
-            buf = &header->buf[offset];
+            int offset = (ringbuf->index + ringbuf->buf_len - 0) % ringbuf->buf_len;
+            buf = &ringbuf->buf[offset];
             Tune &ins = Tune::getInstance();
             ins.read_access_data((struct uncore_scenario *)buf->data, buf->len);
             ins.migration();
-            g_scenario_buf_cnt = header->count;
+            g_scenario_buf_cnt = ringbuf->count;
         }
     }
 }
 
-
-struct TuneInterface tune_interface = {
+struct Interface tune_interface = {
     .get_version = get_version,
     .get_name = get_name,
     .get_description = get_description,
     .get_dep = get_dep,
-    .get_cycle = get_cycle,
+    .get_priority = get_priority,
+    .get_type = nullptr,
+    .get_period = get_period,
     .enable = enable,
     .disable = disable,
-    .tune = tune,
+    .get_ring_buf = nullptr,
+    .run = run,
 };
-extern "C" int get_instance(TuneInterface * *ins)
+
+extern "C" int get_instance(struct Interface **interface)
 {
-    *ins = &tune_interface;
+    *interface = &tune_interface;
     return 1;
 }
-
